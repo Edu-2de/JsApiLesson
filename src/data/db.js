ee061acbe83217d50739decc19c6f510f6bea838
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-06-03 15:17:09 -0300

    feat: initialize authors and books data in the database

--- src/data/db.js
+++ src/data/db.js
@@ -1,4 +1,13 @@
-const autores = [];
-const livros = [];
+const autores = [
+    { id: 1, nome: "Machado de Assis" },
+    { id: 2, nome: "Clarice Lispector" },
+    { id: 3, nome: "Jorge Amado" }
+];
+
+const livros = [
+    { id: 1, titulo: "Dom Casmurro", autorId: 1 },
+    { id: 2, titulo: "A Hora da Estrela", autorId: 2 },
+    { id: 3, titulo: "Capitães da Areia", autorId: 3 }
+];
 
 module.exports = { autores, livros };
